681e1660a1262f2b0c94a566450f02b95bb565c7
IlyaDOC <ilya22@inbox.ru>
2024-10-24 11:06:01 +0300

    added header and sidebar

--- src/assets/js/header.js
+++ src/assets/js/header.js
@@ -1,21 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
-
+    headerAction();
 });
-const navMenu = document.getElementById("navMenu");
+
+const navMenu = document.querySelector('.header__nav');
+const navbarClose = document.getElementById('navbarClose');
+const navbarJSWrapper = document.createElement('div');
+navbarJSWrapper.setAttribute('id', 'navbarJSWrapper');
+navbarJSWrapper.classList.add('navbar-js');
+
 function headerAction() {
     const navbarToggler = document.getElementById("navbarToggler");
     navbarPrepare();
     navbarToggler.addEventListener("click", () => {
-
+        navbarJSWrapper.classList.add('opened');
     });
+
+    navbarClose.addEventListener('click', () => {
+        navbarJSWrapper.classList.remove('opened');
+    });
+
 }
 
 function navbarPrepare() {
     const headerLogo = document.querySelector('.header__logo');
     const headerContacts = document.querySelector('.header__contacts');
 
+
     if (navMenu && headerLogo && headerContacts) {
-        navMenu.insertBefore(headerLogo.cloneNode(true), navMenu.firstChild);
-        navMenu.appendChild(headerContacts.cloneNode(true));
+        document.getElementById('header').appendChild(navbarJSWrapper);
+        navbarJSWrapper.appendChild(headerLogo.cloneNode(true));
+        navbarJSWrapper.appendChild(navMenu.cloneNode(true));
+        navbarJSWrapper.appendChild(headerContacts.cloneNode(true));
+        navbarJSWrapper.appendChild(navbarClose);
     }
 }
